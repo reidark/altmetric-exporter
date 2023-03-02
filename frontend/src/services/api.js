fb2020132ba1b6fbf3c8197bc8cc969c608cc500
Victor Hugo Matias <victormatiasrei@gmail.com>
2023-03-02 01:11:02 -0300

    Update packages versions and backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'https://altmetric-exporter-backend.herokuapp.com'
+  baseURL:
+    process.env.API_URL || "https://altmetric-exporter-backend.onrender.com",
 });
 
 export default api;
